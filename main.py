79abe87b5d3461714f18737c44064094987e48de
souravbhaumik <45196001+souravbhaumik@users.noreply.github.com>
2021-10-31 21:58:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,47 +28,38 @@ scissors = """
 ---.__(___)
 """
 
+game_images = [rock, paper, scissors]
 try:
 
     while True:
         try:
-            r_p_s = int(input("Enter 1 for Rock, 2 for Paper, 3 for Scissors.\n"))
-            r_p_s_computer = random.randint(1, 3)
+            r_p_s = int(input("Enter 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+            r_p_s_computer = random.randint(0, 2)
 
           
-            if r_p_s == 1 or r_p_s == 2 or r_p_s == 3:
+            if r_p_s == 0 or r_p_s == 1 or r_p_s == 2:
 
-                if r_p_s == 1:
-                    print(rock)
-                elif r_p_s == 2:
-                    print(paper)
-                elif r_p_s == 3:
-                    print(scissors)
-                else:
-                    print("Please choose from given numbers. Enter 1 for Rock, 2 for Paper, 3 for Scissors.\n")
+                
+                print(game_images[r_p_s])
 
-                if r_p_s_computer == 1:
-                    print(rock)
-                if r_p_s_computer == 2:
-                    print(paper)
-                if r_p_s_computer == 3:
-                    print(scissors)
-                else:
-                    pass
+                
+                print(game_images[r_p_s_computer])
 
                 if r_p_s == r_p_s_computer:
                     print("Match Draw.\n")
-                elif r_p_s == 2 and r_p_s_computer == 1:
+                elif r_p_s == 1 and r_p_s_computer == 0:
                     print("You win!\n")
-                elif r_p_s == 2 and r_s_computer == 3:
+                elif r_p_s == 1 and r_p_s_computer == 2:
                     print("You win!\n")
-                elif r_p_s == 3 and r_s_computer == 0:
+                elif r_p_s == 2 and r_p_s_computer == 0:
                     print("You win!\n")
                 else:
                     print("You lose!\nTry again!")
             else:
                 print("Please enter correct input")
         except ValueError:
+            print("Please enter between the numbers given bellow.")
+        except IndexError:
             print("Please enter between the numbers given bellow.")
             
 
